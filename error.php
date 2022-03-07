3629b876df7dd36f3e54fe5041025b0c9f8b06bc
iamaximebrousse <78874403+iamaximebrousse@users.noreply.github.com>
2022-03-07 12:53:25 +0000

    Branding of the website by changing the titles

--- error.php
+++ error.php
@@ -3,21 +3,22 @@
 
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>Portfolio 2022 : Error</title>
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <!-- the body section -->
 <body>
-    <header><h1>My Guitar Shop</h1></header>
-
+    <header>
+        <h1>Error on the page, try again</h1>
+    </header>
     <main>
         <h2 class="top">Error</h2>
         <p><?php echo $error; ?></p>
     </main>
 
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+        <p>&copy; <?php echo date("Y"); ?> Portfolio 2022, Inc.</p>
     </footer>
 </body>
 </html>
